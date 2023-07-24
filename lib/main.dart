cf547f77d9ab34ef99b6acfadd2ec9c7492129f6
Geass <64652568+Geass@users.noreply.github.com>
2023-07-24 19:41:43 +0200

    Update main.dart
    - dodany przycisk którym tworzymy notatki przez co aplikacja wygląda lepiej.
    - możliwość dodania tytułu oraz treści notatki.

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _todoList = <String>[];
-  final _textController = TextEditingController();
+  final _todoList = <Map<String, String>>[];
 
   @override
   Widget build(BuildContext context) {
@@ -34,43 +33,124 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: ListView.builder(
-              itemCount: _todoList.length,
-              itemBuilder: (BuildContext context, int index) {
-                final todo = _todoList[index];
-                return Dismissible(
-                  key: Key(todo),
-                  onDismissed: (direction) {
-                    setState(() {
-                      _todoList.removeAt(index);
-                    });
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text("$todo dismissed")));
-                  },
-                  background: Container(color: Colors.red),
-                  child: ListTile(title: Text(todo)),
+      body: ListView.builder(
+        itemCount: _todoList.length,
+        itemBuilder: (BuildContext context, int index) {
+          final todo = _todoList[index];
+          return Dismissible(
+            key: Key(todo['title']!),
+            onDismissed: (direction) {
+              setState(() {
+                _todoList.removeAt(index);
+              });
+              ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text("${todo['title']} dismissed")));
+            },
+            background: Container(color: Colors.red),
+            child: ListTile(
+              title: Text(todo['title']!),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => NoteDetailPage(note: todo),
+                  ),
                 );
               },
             ),
-          ),
-          TextField(
-            controller: _textController,
-            decoration: InputDecoration(
-              hintText: 'Enter task',
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          // Navigate to the AddNotePage when the button is tapped
+          final result = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AddNotePage()),
+          );
+          // Add the new note to the list if it is not null
+          if (result != null) {
+            setState(() {
+              _todoList.add({'title': result[0], 'content': result[1]});
+            });
+          }
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class AddNotePage extends StatefulWidget {
+  @override
+  _AddNotePageState createState() => _AddNotePageState();
+}
+
+class _AddNotePageState extends State<AddNotePage> {
+  final _titleController = TextEditingController();
+  final _contentController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Note'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(
+                labelText: 'Title',
+              ),
             ),
-            onSubmitted: (text) {
-              setState(() {
-                if (text.isNotEmpty) {
-                  _todoList.add(text);
-                  _textController.clear();
-                }
-              });
-            },
-          ),
-        ],
+            TextField(
+              controller: _contentController,
+              decoration: InputDecoration(
+                labelText: 'Content',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Save the note and navigate back to the home page
+                Navigator.pop(
+                    context, [_titleController.text, _contentController.text]);
+              },
+              child: Text('Save'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NoteDetailPage extends StatelessWidget {
+  final Map<String, String> note;
+
+  NoteDetailPage({Key? key, required this.note}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(note['title']!),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              note['title']!,
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            SizedBox(height: 8.0),
+            Text(note['content']!),
+          ],
+        ),
       ),
     );
   }
